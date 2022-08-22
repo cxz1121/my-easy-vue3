986e8010d84c29fef88eb1ab50b4019ac194f643
cxz1121 <1217044926@qq.com>
2022-08-22 23:46:10 +0800

    isProxy test

--- src/reactivity/test/reactive.spec.ts
+++ src/reactivity/test/reactive.spec.ts
@@ -1,14 +1,16 @@
-import { isReactive, reactive } from '../reactive'
+import { isProxy, isReactive, reactive, readonly } from '../reactive'
 
 describe('reactive', () => {
   it('happy path', () => {
     const data = { age: 33 }
     const state = reactive(data)
-
+    const readonlyState = readonly(data)
     expect(state).not.toBe(data)
     expect(state.age).toBe(33)
     expect(isReactive(state)).toBe(true)
     expect(isReactive(data)).toBe(false)
+    expect(isProxy(state)).toBe(true)
+    expect(isProxy(readonlyState)).toBe(true)
   })
 
   it('nested reactive', () => {
